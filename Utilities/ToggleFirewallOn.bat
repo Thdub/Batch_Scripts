6e155ef41f98a9b7632696139bac64f2c87a55fd
Thomas Dubreuil <37347541+Thdub@users.noreply.github.com>
2019-06-12 15:24:57 +0200

    Add files via upload

---
+++ Utilities/ToggleFirewallOn.bat
@@ -0,0 +1,13 @@
+@echo off
+
+%windir%\system32\whoami.exe /USER | find /i "S-1-5-18" 1>nul && (
+goto :OK
+) || (
+NSudoG -U:T -P:E -ShowWindowMode:Hide "%~dpnx0"&exit /b >NUL 2>&1
+)
+
+:OK
+SetACL -on "HKLM\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile" -ot reg -actn setprot -op "dacl:np" -actn clear -clr "dacl" -actn rstchldrn -rst "dacl" >NUL 2>&1
+netsh advfirewall set  currentprofile state on >NUL 2>&1
+SetACL -on "HKLM\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile" -ot reg -actn setprot -op "dacl:p_nc" -actn clear -clr "dacl" -actn ace -ace "n:SYSTEM;p:full" -ace "n:NT SERVICE\mpssvc;p:read" >NUL 2>&1
+exit
