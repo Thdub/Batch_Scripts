0ce24c02d550358c60f2ab6a2e02f83f034b88ca
Thomas Dubreuil <37347541+Thdub@users.noreply.github.com>
2019-06-12 15:23:04 +0200

    Delete ScreenSnipFullScreen.bat

--- ScreenSnipFullScreen.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-timeout /t 1 >NUL 2>&1
-for /f "tokens=1, 2, 3, 4 delims=-/. " %%a in ("%DATE%") do set "ClipName=%%a-%%b-%%c"
-for /f "tokens=1-3 delims=:," %%a in ("%TIME%") do set "ClipName=%ClipName%_%%ah%%bm%%cs"
-"C:\Program Files\System Tools\System Utilities\NirCmd\nircmdc.exe" savescreenshot "%USERPROFILE%\Desktop\%ClipName%.png"
-exit /b
