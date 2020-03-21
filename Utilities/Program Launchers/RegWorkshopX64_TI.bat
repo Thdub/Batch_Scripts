71c68f126f178f2120ae64d8385df43baaf73047
Thomas Dubreuil <37347541+Thdub@users.noreply.github.com>
2020-03-21 15:08:13 +0000

    Add files via upload

--- Utilities/Program Launchers/RegWorkshopX64_TI.bat
+++ Utilities/Program Launchers/RegWorkshopX64_TI.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\System Tools\System Utilities\NSudo\NSudoLC.exe" -U:T -P:E "%PROGRAMFILES%\Registry Workshop\RegWorkshopX64.exe" >nul 2>&1
+NSudoLC.exe -U:T -P:E "%PROGRAMFILES%\Registry Workshop\RegWorkshopX64.exe" >nul 2>&1
 exit /b
