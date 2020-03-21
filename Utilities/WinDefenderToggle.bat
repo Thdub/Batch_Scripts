b517506af3e2b35ffb0da2f9cced1f3fcc11419e
Thomas Dubreuil <37347541+Thdub@users.noreply.github.com>
2020-03-21 14:59:32 +0000

    Add files via upload

--- Utilities/WinDefenderToggle.bat
+++ Utilities/WinDefenderToggle.bat
@@ -1,10 +1,24 @@
 @echo off
 
-cd /d "%~dp0/1"
-sc query WinDefend | find "STATE" | find "RUNNING" && goto :stop
+C:\Windows\system32\whoami.exe /USER | find /i "S-1-5-18" 1>nul && (
+	goto :OK
+) || (
+	NSudoLC -U:T -P:E -Wait -UseCurrentConsole "%~dpnx0" && exit /b
+)
+:OK
+set "PScommand=%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass"
+for /f "tokens=3 delims=: " %%i in ('sc query WinDefend ^| findstr /i "STATE"') do ( set "WinDefend=%%i" )
+if /i "%WinDefend%"=="RUNNING" ( goto :stop )
+if /i "%WinDefend%"=="STOPPED" ( goto :start )
 
 :start
-NSudoG.exe -U:T -ShowWindowMode:Hide sc start WinDefend & exit /b
+sc start WinDefend >nul 2>&1
+:query_Service
+for /f "tokens=3 delims=: " %%i in ('sc query WinDefend ^| findstr /i "STATE"') do ( set "WinDefend2=%%i" )
+if /i "%WinDefend2%"=="RUNNING" ( %PScommand% -c "Set-MpPreference -DisableRealtimeMonitoring $false" )	else ( goto :query_Service )
+exit /b
 
 :stop
-NSudoG.exe -U:T -ShowWindowMode:Hide sc stop WinDefend & exit /b
+%PScommand% -c "Set-MpPreference -DisableRealtimeMonitoring $true"
+sc stop WinDefend >nul 2>&1
+exit /b
